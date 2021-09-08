77304d9e4aa39ce61a4d520651ca2cbe0615242f
Jihanra <jihanrahadatulasy@gmail.com>
2021-09-08 15:18:58 +0700

    Minggu2_Prak2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama Saya Jihan Rahadatul Aisy dengan NIM 2031710034';
+    }
+}
---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        return 'Selamat Datang';
+    }
+
+    public function about() {
+        return 'Nama Saya Jihan Rahadatul Aisy dengan NIM 2031710034';
+    }
+
+    public function articles($id){
+        return 'Halaman Artikel dengan ID '.$id;
+    }
+
+
+}
